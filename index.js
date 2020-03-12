0ac2e3c5a9e2c5fe4d0badc3caaae0578d02242b
Pawel <walek.smigielski@gmail.com>
2020-03-12 19:45:34 +0100

    added obj&mlt loader

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.module.js';
 import {PointerLockControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r113/examples/jsm/controls/PointerLockControls.js';
-var camera, scene, controls, renderer, canvas;
+var camera, scene, controls, renderer, canvas, loadingManager, clock, mesh;
 var velocity = new THREE.Vector3();
 var direction = new THREE.Vector3();
 var objects = [];
@@ -9,18 +8,42 @@ var moveBackward = false;
 var moveLeft = false;
 var moveRight = false;
 var prevTime = performance.now();
+var USE_WIREFRAME = false;
+var loadingManager = null;
+var RESOURCES_LOADED = false;
+
+var models = {
+	house: {
+		obj:"models/house.obj",
+		mtl:"models/house.mtl",
+		mesh: null
+	},
+};
+var meshes = {};
 
 function init() {
   canvas = document.querySelector('#c');
   renderer = new THREE.WebGLRenderer({canvas});
+  clock = new THREE.Clock();
+
 
   const fov = 75;
   const aspect = 2;
   const near = 0.1;
-  const far = 5;
+  const far = 100;
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.y = 2;
 
+  loadingManager = new THREE.LoadingManager();
+	loadingManager.onProgress = function(item, loaded, total){
+		console.log(item, loaded, total);
+	};
+	loadingManager.onLoad = function(){
+		console.log("loaded all resources");
+		RESOURCES_LOADED = true;
+		onResourcesLoaded();
+  };
+  
   scene = new THREE.Scene();
 
   controls = new PointerLockControls(camera);
@@ -37,10 +60,19 @@ function init() {
     controls.addEventListener( 'unlock', function () {
     	blocker.style.display = 'block';
     	instructions.style.display = '';
-    } );
+    } ); 
 
   scene.add(controls.getObject());
 
+  mesh = new THREE.Mesh(
+		new THREE.BoxGeometry(1,1,1),
+		new THREE.MeshPhongMaterial({color:0xff4444, wireframe:USE_WIREFRAME})
+	);
+	mesh.position.y += 1;
+	mesh.receiveShadow = true;
+  mesh.castShadow = true;
+  
+	scene.add(mesh);
   //MOVING CONTROLS
   var onKeyDown = function ( event ) {
      switch ( event.keyCode ) {
@@ -92,7 +124,38 @@ function init() {
 
   document.addEventListener( 'keydown', onKeyDown, false );
   document.addEventListener( 'keyup', onKeyUp, false );
-
+  //loading models
+for( var _key in models ){
+		(function(key){
+			var mtlLoader = new THREE.MTLLoader(loadingManager);
+			mtlLoader.load(models[key].mtl, function(materials){
+				materials.preload();
+				
+				var objLoader = new THREE.OBJLoader(loadingManager);
+				
+				objLoader.setMaterials(materials);
+				objLoader.load(models[key].obj, function(mesh){
+					
+					mesh.traverse(function(node){
+						if( node instanceof THREE.Mesh ){
+							if('castShadow' in models[key])
+								node.castShadow = models[key].castShadow;
+							else
+								node.castShadow = true;
+							
+							if('receiveShadow' in models[key])
+								node.receiveShadow = models[key].receiveShadow;
+							else
+								node.receiveShadow = true;
+						}
+					});
+					models[key].mesh = mesh;
+					
+				});
+			});
+			
+		})(_key);
+	}
   //LIGHT
   const color = 0xFFFFFF;
   const intensity = 1;
@@ -140,6 +203,14 @@ function init() {
   }
   requestAnimationFrame(render);
 }
+function onResourcesLoaded(){
+	// Clone models into meshes.
+	meshes["house"] = models.house.mesh.clone();
+	
+	// Reposition individual meshes, then add meshes to scene
+	meshes["house"].position.set(-5, 0, 4);
+	scene.add(meshes["house"]);
+} 
 
 function animate(){
     requestAnimationFrame(animate);
